b097e0b6b302ea2d37da040d22fa3643abd553cb
haruki_murasaki <haruki_murasaki@haruki-murasakinoMacBook-Pro.local>
2020-11-28 22:01:00 +0900

    fix icon

--- src/components/pages/App/index.tsx
+++ src/components/pages/App/index.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { AuthContextProvider } from "../../contexts/AuthContext";
+import { AppNavigator } from "../../navigation/AppNavigator";
 
 export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContextProvider>
+      <AppNavigator />
+    </AuthContextProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
